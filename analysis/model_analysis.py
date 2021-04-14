f842dd1233abcafb22119c2800090f31399e3bc2
Michael Mondoro <michaelmondoro@Michaels-MacBook-Air.local>
2021-04-14 10:49:34 -0400

    Initialized repo.

---
+++ analysis/model_analysis.py
@@ -0,0 +1,45 @@
+import json
+import numpy as np
+import cv2
+import os
+import pickle
+
+# Load model
+f = open('full_dataset_model.sav','rb')
+m = pickle.load(f)
+# Load keypoints
+keypoint_path = "output/"
+keypoints = []
+for file in os.listdir(keypoint_path):
+    f = open(keypoint_path + file)
+    data = json.load(f)
+    if data['people'] != []:
+        keypoints.append(np.array( [data['people'][0]['pose_keypoints_2d']] ))
+    
+    
+
+# Play video while analyzing each frame
+cap = cv2.VideoCapture("out.avi")
+frame_width = int(cap.get(3)) 
+frame_height = int(cap.get(4)) 
+   
+size = (frame_width, frame_height)
+result = cv2.VideoWriter('keypoint_out.avi',  
+                         cv2.VideoWriter_fourcc(*'MJPG'), 
+                         10, size) 
+
+frame_ndx = 0
+while True:
+    if cap.grab():
+        flag, frame = cap.retrieve()
+        pred = str(m.predict(keypoints[frame_ndx]))
+        cv2.putText(frame,pred,(10,500), cv2.FONT_HERSHEY_SIMPLEX, 1,(255,255,255),2)
+        result.write(frame)
+        if not flag:
+            continue
+        else:
+            cv2.imshow('video', frame)
+    if cv2.waitKey(10) == 27:
+        break
+    
+    frame_ndx += 1
---
+++ save.py
@@ -0,0 +1,48 @@
+import cv2
+import os
+
+
+'''
+Saves video to /output folder.
+Used to gather training data.
+
+version 10.20.19
+'''
+def save():
+    cam = cv2.VideoCapture(1)
+
+    
+    path, dirs, files = next(os.walk("output"))
+    file_count = len(files)
+    print(file_count)
+
+    # Set frame width and height 
+    frame_width = int(cam.get(3)/2)
+    frame_height = int(cam.get(4))
+
+    # Define the codec and filename.
+    fourcc = cv2.VideoWriter_fourcc('M','J','P','G')  # cv2.VideoWriter_fourcc() does not exist
+    out = cv2.VideoWriter("output/output{}.avi".format(file_count+1), fourcc, 24, (frame_width, frame_height))
+    while (cam.isOpened()):
+        ret_val, img = cam.read()
+    
+        # Edit video
+        h,w = img.shape[:2]
+        cropped = img[:, :int(w/2)]
+        final_image = cv2.flip(cropped, -1)
+        
+        # Display Image
+        out.write(final_image)
+        
+        cv2.imshow("Frame", final_image)
+        
+        if cv2.waitKey(1) == 27: 
+            break  # esc to quit
+
+    cam.release()
+    cv2.destroyAllWindows()
+    out.release()
+
+
+if __name__ == '__main__':
+    save()
